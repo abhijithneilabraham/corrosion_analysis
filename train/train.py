52f907f3becf2faa5213f1e4635c1e2408d9a540
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-05-01 22:50:46 +0530

    crime

---
+++ train/train.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  1 22:10:58 2020
+
+@author: abhijithneilabraham
+"""
+
+import pandas as pd
+from pandas import DataFrame
+from pandas import concat
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from matplotlib import pyplot
+from math import sqrt
+from numpy import random
+random.seed(7)
+
+def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
+	"""
+	Frame a time series as a supervised learning dataset.
+	Arguments:
+		data: Sequence of observations as a list or NumPy array.
+		n_in: Number of lag observations as input (X).
+		n_out: Number of observations as output (y).
+		dropnan: Boolean whether or not to drop rows with NaN values.
+	Returns:
+		Pandas DataFrame of series framed for supervised learning.
+	"""
+	n_vars = 1 if type(data) is list else data.shape[1]
+	df = DataFrame(data)
+	cols, names = list(), list()
+	# input sequence (t-n, ... t-1)
+	for i in range(n_in, 0, -1):
+		cols.append(df.shift(i))
+		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+	# forecast sequence (t, t+1, ... t+n)
+	for i in range(0, n_out):
+		cols.append(df.shift(-i))
+		if i == 0:
+			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+		else:
+			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+	# put it all together
+	agg = concat(cols, axis=1)
+	agg.columns = names
+	# drop rows with NaN values
+	if dropnan:
+		agg.dropna(inplace=True)
+	return agg
+data =  pd.read_excel(r'datasheet.xlsx',sheet_name="data1")
+y=list(data["OVERALL CONDITION"][1:])
+x1=list(data["METAL LOSS"][1:])
+x2=list(data["CATHODIC PROTECTION "][1:])
+
+df=pd.DataFrame({"METAL LOSS":x1,"CATHODIC PROTECTION":x2,"OVERALL CONDITION":y})
+values=df.values
+values = values.astype('float32')
+# normalize features
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+x=scaled[:,:2]
+y=scaled[:,2]
+model = Sequential()
+model.add(Dense(12, input_dim=2, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='softmax'))
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(x, y, epochs=150, batch_size=10)
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
+
+
